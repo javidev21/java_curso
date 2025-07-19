440166a174c54ea4bb2b08cfb4f86cc25a6a86f8
javidev21 <ingjaviersalas@gmail.com>
2025-07-18 22:58:26 -0600

    Actualizar Controlador y propiedades

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,5 +1,18 @@
 package com.example.demo;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller 
+public class IndexController {
+	
+	//Es el controlador de la fiesta.
+	//Le vas a pasar la vista que quieres recibir.
+	//Vamos a usar el tema de los models.
+	@GetMapping({"/index", "/", "/home"})
+	public String index(Model model) {
+		return "index";
+	}
 }
+
