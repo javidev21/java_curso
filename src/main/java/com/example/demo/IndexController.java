287b8717ece2ccce12c3fc8d7306d10899e512e4
javidev21 <ingjaviersalas@gmail.com>
2025-07-20 21:06:58 -0600

    Actualizar controlador y propiedades

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller 
 public class IndexController {
@@ -14,5 +16,20 @@ public class IndexController {
 	public String index(Model model) {
 		return "index";
 	}
+	@GetMapping({"/catalogo", "/catalog"})
+	public String Catalogo(Model model){
+		return "catalogo";
+	}
+	@GetMapping({"/about"})
+	public String About(Model model) {
+		return "about";
+	}
+	@GetMapping({"/Bot", "/bot"})
+	public String Bot(Model model){
+		return "index";
+	}
+	
+	}
+	
 }
 
